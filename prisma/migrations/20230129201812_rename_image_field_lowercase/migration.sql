6b3186a57a8170e35a9bb3234481bc0dc28d7aef
Andrei Filip <andreifilip123@gmail.com>
2023-01-29 23:05:23 +0200

    Add uploaded images to user

---
+++ prisma/migrations/20230129201812_rename_image_field_lowercase/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `image` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" DATETIME
+);
+INSERT INTO "new_User" ("email", "emailVerified", "id", "name") SELECT "email", "emailVerified", "id", "name" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
