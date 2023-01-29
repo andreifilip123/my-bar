ee5893b99d56e20ab8b82a3165542aaff2ed6702
Andrei Filip <andreifilip123@gmail.com>
2023-01-29 09:25:50 +0200

    Disable single quotes

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
   semi: true,
   trailingComma: "all",
-  singleQuote: true,
+  singleQuote: false,
   jsxSingleQuote: false,
   bracketSpacing: true,
   bracketSameLine: false,
