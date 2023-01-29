cd356b950ade0da1117fc2d8e336d6a7292251cf
Andrei Filip <andreifilip123@gmail.com>
2023-01-29 09:22:06 +0200

    Setup prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,10 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
 };
