e0af961ef44f31b6a16b9d2ecf58dfca1305e766
Andrei Filip <andreifilip123@gmail.com>
2023-01-29 23:13:47 +0200

    Add image upload

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -9,9 +9,10 @@ import {
   Td,
   Th,
   Thead,
-  Tr
+  Tr,
 } from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
+import type { PresignedPost } from "aws-sdk/clients/s3";
 import type { NextPage } from "next";
 import { Fragment } from "react";
 import type { SubmitHandler } from "react-hook-form";
@@ -22,6 +23,7 @@ import { api } from "../../utils/api";
 
 interface IFormInputs {
   name: string;
+  image: FileList;
   ingredients: Array<{
     amount: number;
     unit: string;
@@ -29,17 +31,32 @@ interface IFormInputs {
   }>;
 }
 
+const MAX_FILE_SIZE = 10 * 1024 * 1024;
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/png", "image/webp"];
+
 const formSchema = z.object({
   name: z.string(),
+  image: z
+    .any()
+    .refine(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      (files) => files[0]?.size <= MAX_FILE_SIZE,
+      `File size should be less than ${MAX_FILE_SIZE / 1024 / 1024}MB.`,
+    )
+    .refine(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+      (files) => ACCEPTED_IMAGE_TYPES.includes(files[0]?.type),
+      "Only .jpg, .jpeg, .png and .webp formats are supported.",
+    ),
   ingredients: z.array(
     z.object({
       amount: z.number().min(0, { message: "Amount should be positive" }),
       unit: z
         .string()
         .min(1, { message: "Unit must be at least 1 character long" }),
-      name: z
-        .string()
-        .min(1, { message: "Ingredient name must be at least 1 character long" }),
+      name: z.string().min(1, {
+        message: "Ingredient name must be at least 1 character long",
+      }),
     }),
   ),
 });
@@ -49,6 +66,7 @@ const Admin: NextPage = () => {
 
   const createCocktail = api.cocktail.create.useMutation();
   const deleteCocktail = api.cocktail.delete.useMutation();
+  const createPresignedUrl = api.aws.createPresignedUrl.useMutation();
 
   const { register, handleSubmit, control, formState, reset } =
     useForm<IFormInputs>({
@@ -67,10 +85,35 @@ const Admin: NextPage = () => {
   });
 
   const onSubmit: SubmitHandler<IFormInputs> = async (data: IFormInputs) => {
+    if (!data.image[0]) return;
+
     try {
+      const presigned = (await createPresignedUrl.mutateAsync({
+        fileName: data.image[0]?.name,
+        fileType: data.image[0]?.type,
+      })) as PresignedPost;
+      const { url, fields } = presigned;
+
+      const imageKey = fields.key as string;
+      const imageId = imageKey.split("/")[1] as string;
+
+      const formData = new FormData();
+
+      Object.entries({ ...fields, file: data.image[0] }).forEach(
+        ([key, value]) => {
+          formData.append(key, value as string | Blob);
+        },
+      );
+
+      await fetch(url, {
+        method: "POST",
+        body: formData,
+      });
+
       await createCocktail.mutateAsync(
         {
           name: data.name,
+          imageId,
           ingredients: data.ingredients.map((ingredient) => ({
             amount: ingredient.amount,
             unit: {
@@ -175,6 +218,8 @@ const Admin: NextPage = () => {
         <Button onClick={() => append({ name: "", amount: 0, unit: "" })}>
           Add ingredient
         </Button>
+
+        <Input type="file" multiple={false} {...register("image")} />
 
         <Input type="submit" />
       </form>
