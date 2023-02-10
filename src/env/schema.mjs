58d59bd2847b1a8b7963894d985564c0ac21c329
Andrei Filip <andreifilip123@gmail.com>
2023-02-10 15:12:51 +0200

    Remove database url env var

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,7 +6,6 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
   POSTGRES_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET:
@@ -34,7 +33,6 @@ export const serverSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof serverSchema>]: z.infer<typeof serverSchema>[k] | undefined }}
  */
 export const serverEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
   POSTGRES_URL: process.env.POSTGRES_URL,
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
