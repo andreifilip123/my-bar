d63a60eb012916d1bd89dc4ff93fd6dfa9bf0568
Andrei Filip <andreifilip123@gmail.com>
2023-03-17 12:48:05 +0200

    Remove remaining useEffect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { Analytics } from "@vercel/analytics/react";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { useEffect } from "react";
 
 import { api } from "../utils/api";
 
