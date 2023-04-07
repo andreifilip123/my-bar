c12c52761dce7165a46fc8df51b24b620eb01a31
Andrei Filip <andreifilip123@gmail.com>
2023-04-07 19:22:55 +0200

    Add cron job to delete all images with missing matching db image every friday at 8pm

---
+++ src/pages/api/cron.ts
@@ -0,0 +1,64 @@
+import {
+  DeleteObjectCommand,
+  ListObjectsV2Command,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { serverEnv } from "../../env/schema.mjs";
+
+const s3 = new S3Client({
+  region: serverEnv.REGION,
+  credentials: {
+    accessKeyId: serverEnv.ACCESS_KEY ?? "",
+    secretAccessKey: serverEnv.SECRET_ACCESS_KEY ?? "",
+  },
+});
+
+const deleteImage = async (imageId?: string) => {
+  const deleteCommand = new DeleteObjectCommand({
+    Bucket: serverEnv.BUCKET_NAME,
+    Key: imageId,
+  });
+  await s3.send(deleteCommand);
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (!prisma) {
+    res.status(500).end("Prisma not initialized");
+    return;
+  }
+  // delete all images from s3 that don't have a corresponding image in the db
+  const listCommand = new ListObjectsV2Command({
+    Bucket: serverEnv.BUCKET_NAME,
+  });
+  const images = await s3.send(listCommand);
+
+  if (!images.Contents) {
+    res.status(200).end("No images to delete");
+    return;
+  }
+
+  const dbImages = await prisma.image.findMany();
+
+  const s3Images = images.Contents.map((image) => image.Key);
+
+  const imagesToDelete = s3Images.filter(
+    (s3Image) => !dbImages.find((dbImage) => dbImage.id === s3Image),
+  );
+
+  if (imagesToDelete.length === 0) {
+    res.status(200).end("No images to delete");
+    return;
+  }
+
+  await Promise.all(
+    imagesToDelete.map(async (imageToDelete) => {
+      await deleteImage(imageToDelete);
+    }),
+  );
+  res.status(200).end(`Deleted ${imagesToDelete.length} images`);
+}
