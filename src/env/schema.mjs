9a4ff8206c5ef67679de9710e83b5f3e0a4a4663
Andrei Filip <andreifilip123@gmail.com>
2023-04-07 14:48:09 +0200

    Remove next-auth secrets

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -9,15 +9,11 @@ export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   CLERK_SECRET_KEY: z.string(),
-  GITHUB_CLIENT_ID: z.string(),
-  GITHUB_CLIENT_SECRET: z.string(),
   BUCKET_NAME: z.string(),
   REGION: z.string(),
   ACCESS_KEY: z.string(),
   SECRET_ACCESS_KEY: z.string(),
   OPENAI_API_KEY: z.string(),
-  EMAIL_SERVER: z.string(),
-  EMAIL_FROM: z.string(),
 });
 
 /**
@@ -29,15 +25,11 @@ export const serverEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   BUCKET_NAME: process.env.BUCKET_NAME,
   REGION: process.env.REGION,
   ACCESS_KEY: process.env.ACCESS_KEY,
   SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  EMAIL_SERVER: process.env.EMAIL_SERVER,
-  EMAIL_FROM: process.env.EMAIL_FROM,
 };
 
 /**
